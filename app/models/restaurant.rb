d854cc8aa3510d84e029053cc02db6fc89da6abf
Steve Watros <steve.watros@gmail.com>
2021-08-25 17:00:16 +0200

    css

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,21 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  CATEGORIES = %w(japanese french chinese italian belgian)
 
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
-  validates :category, inclusion: { in: %w(japanese french chinese italian belgian),
-    message: "%{value} is not a valid category" }
+  validates :category, inclusion: { in: CATEGORIES }
+
+  def average_rating
+    sum = 0
+    self.reviews.each do |review|
+      sum += review.rating
+    end
+    if self.reviews.length == 0
+      "-"
+    else
+      sum.to_f / self.reviews.length
+    end
+  end
 end
